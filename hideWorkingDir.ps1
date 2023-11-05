5647fb4639f561576d699900640b567a94bf56d8
Simphiwe Magagula <simphiweiq@gmail.com>
2023-11-05 08:40:10 +0200

    Delete hideWorkingDir.ps1

--- hideWorkingDir.ps1
+++
@@ -1,6 +0,0 @@
-function Prompt
- {                                                                      
->>     "PS $($executionContext.SessionState.Path.CurrentLocation.Path.Split('\')[-1])$('>' * ($nestedPromptLevel
- +1)) "
->> }
-
